97c008bb82c35a3ff715c975784b1990658b1fd2
N0raan <105749091+N0raan@users.noreply.github.com>
2023-03-08 18:23:19 +0200

    Create ChatBasics.java
    interface class of the basic methods of product

---
+++ Builder pattern/ChatBasics.java
@@ -0,0 +1,8 @@
+package Tutorial;
+
+public interface ChatBasics {
+
+	public void SetNotificationSound(String notification);
+	public void SetParticipantsNumber(int Participants);
+	public void SetName(String name);
+}
